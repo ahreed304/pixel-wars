06a682fda626018fb764d164104b12fa6a6e6456
Emma Dublin <emmadublin@Emmas-MacBook-Pro-8.local>
2022-04-16 12:58:53 -0500

    fixed db bugs

--- code/generate_database.py
+++ code/generate_database.py
@@ -1,13 +1,30 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import declarative_base
 import psycopg2
+
+table_name = 'pixel_placement'
+
+Base = declarative_base()
+
+class Pixel_placement(Base):
+    __tablename__ = table_name
+
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime)
+    user_id = Column(String)
+    pixel_color = Column(String)
+    x = Column(Integer)
+    y = Column(Integer)
 
 conn_string = 'postgresql+psycopg2://postgres:pass@127.0.0.1/reddit'
 db = create_engine(conn_string)
 
-conn = psycopg2.connect("dbname=reddit user=postgres password=foo")
+conn = psycopg2.connect("dbname=reddit user=postgres password=admin")
 conn.autocommit = True
 cursor = conn.cursor()
+
+Base.metadata.create_all(db)
 
 prefix = 'https://placedata.reddit.com/data/canvas-history/2022_place_canvas_history-0000000000'
 extension = '.csv.gzip'
@@ -22,8 +39,17 @@ for i in range(0, 79):
     df = df.drop('coordinate', axis=1)
 
     print(f'Loading into database...    ', end='', flush=True)
-    df.to_sql('pixel_placement3', db, if_exists='append')
+    df.to_sql(table_name, db, if_exists='append', index=False)
     conn.commit()
     print(f'Done')
 
 conn.close()
+
+
+
+
+
+
+
+
+
